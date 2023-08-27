6a01a55e0b5a7ab88d4ef4821fa7f6ce91b9cda1
choiseyeon <choi20014830@gmail.com>
2023-08-27 18:33:12 +0900

    fix: 그리드 설정

--- frontend/src/MovieList.svelte
+++ frontend/src/MovieList.svelte
@@ -9,11 +9,35 @@
 
 </script>
 
-{#each movies as movie}
-    <div role="button"
-         tabindex="0"
-         on:click={() => selectMovie(movie)}
-         on:keydown={(e) => { if (e.key === 'Enter') selectMovie(movie); }}>
-        {movie.title}
-    </div>
-{/each}
+<!--<ul>-->
+<!--    {#each movies as movie}-->
+<!--        <li class="movie-list-item" role="button" tabindex="0"-->
+<!--            on:click={() => selectMovie(movie)}-->
+<!--            on:keydown={(e) => { if (e.key === 'Enter') selectMovie(movie); }}>-->
+<!--            <span>{movie.title}</span>-->
+<!--        </li>-->
+<!--    {/each}-->
+<!--</ul>-->
+
+<style>
+    .movie-grid {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        gap: 16px;
+    }
+    .movie-item {
+        padding: 16px;
+        border: 1px solid #ccc;
+        text-align: center;
+    }
+</style>
+
+<div class="movie-grid">
+    {#each movies as movie}
+        <div class="movie-item" role="button" tabindex="0"
+             on:click={() => selectMovie(movie)}
+             on:keydown={(e) => { if (e.key === 'Enter') selectMovie(movie); }}>
+            <h3>{movie.title}</h3>
+        </div>
+    {/each}
+</div>
