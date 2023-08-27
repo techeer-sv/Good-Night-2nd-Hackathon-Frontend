2ea48717d833db85889be39c0bb80aa89d2d8735
HyunTaek Oh <oht222@naver.com>
2023-08-27 22:19:27 +0900

    add: GNB에 드롭다운 페이지 이동 추가

--- src/components/GNB.tsx
+++ src/components/GNB.tsx
@@ -1,7 +1,28 @@
 import { useRouter } from "next/router";
+import {
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+} from "@nextui-org/react";
+import React from "react";
 
 function GNB() {
   const router = useRouter();
+
+  const items = [
+    {
+      key: "seeMovieList",
+      label: "영화 목록 보기",
+      value: "/",
+    },
+    {
+      key: "addMovie",
+      label: "영화 추가",
+      value: "/movie/add",
+    },
+  ];
 
   return (
     <div className="mx-10 flex h-24 max-w-full place-items-center tablet:w-full tablet:max-w-screen-xl">
@@ -22,6 +43,26 @@ function GNB() {
           GoodNight 2nd Hackathon
         </button>
       </nav>
+      <div className="flex h-full place-items-center justify-end tablet:hidden">
+        <Dropdown className="w-full">
+          <DropdownTrigger>
+            <Button className="font-semibold">이동하기</Button>
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Dynamic Actions" items={items}>
+            {items.map((item) => (
+              <DropdownItem
+                id={item.key.toString()}
+                key={item.key}
+                value={item.value}
+                className="text-black"
+                onClick={() => router.push(item.value)}
+              >
+                {item.label}
+              </DropdownItem>
+            ))}
+          </DropdownMenu>
+        </Dropdown>
+      </div>
     </div>
   );
 }
