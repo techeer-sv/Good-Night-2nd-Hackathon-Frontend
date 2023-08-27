e7c48c3e7f1637feb6a466b592ba642c73762e09
choiseyeon <choi20014830@gmail.com>
2023-08-27 15:48:03 +0900

    feat: 영화 목록 페이지

---
+++ frontend/src/MovieList.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    export let movies: { title: string }[];
+
+    const selectMovie = (movie: { title: string, year: number }) => {
+        alert(`You selected ${movie.title}`);
+    };
+</script>
+
+<ul>
+    {#each movies as movie}
+        <li>
+            {movie.title}
+            <button on:click={() => selectMovie(movie)}>Select</button>
+        </li>
+    {/each}
+</ul>
---
+++ frontend/src/routes/movie/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import MovieList from '../../MovieList.svelte';
+
+    const movies = [
+        { title: 'Inception', year: 2010 },
+        { title: 'The Matrix', year: 1999 },
+        { title: 'Interstellar', year: 2014 }
+    ];
+</script>
+
+<main>
+    <h1>Movie List</h1>
+    <MovieList {movies} />
+</main>
