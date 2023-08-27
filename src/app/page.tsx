61fc9dfc8a110dd3f0e6467365fa54de520cc814
Mayreeel <kuwon15@gmail.com>
2023-08-27 19:16:04 +0900

    feat : 최소 별점 필터 및 페이지네이션 기능 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,11 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
+type Data = {
+  movies: Movie[];
+  total: number;
+};
+
 type Movie = {
   id: number;
   title: string;
@@ -13,15 +18,18 @@ type Movie = {
   createdAt: string;
   updatedAt: string;
   deletedAt: string | null;
+  averageRating: number;
 };
 
 export default function Home() {
   const [genre, setGenre] = useState<string>("");
   const [isShowing, setIsShowing] = useState<string>("");
-  const [data, setData] = useState<Movie[]>([]);
+  const [data, setData] = useState<Data>({ movies: [], total: 0 });
+  const [minRating, setMinRating] = useState<number>(0);
+  const [page, setPage] = useState<number>(1);
 
   async function getMovies() {
-    const url = new URL("http://localhost:8000/movies");
+    const genreIsShowingurl = new URL("http://localhost:8000/movies");
     const params = new URLSearchParams();
 
     if (genre) {
@@ -32,10 +40,13 @@ export default function Home() {
       params.set("isShowing", isShowing);
     }
 
-    url.search = params.toString();
+    params.set("page", page.toString());
+    params.set("itemsPerPage", "8");
+
+    genreIsShowingurl.search = params.toString();
 
     try {
-      const dataRes = await fetch(url.toString());
+      const dataRes = await fetch(genreIsShowingurl.toString());
       if (!dataRes.ok) {
         throw new Error(`HTTP error! Status: ${dataRes.status}`);
       }
@@ -73,13 +84,17 @@ export default function Home() {
     setIsShowing(e.target.value);
   };
 
+  const handleMinRatingChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setMinRating(Number(e.target.value));
+  };
+
   useEffect(() => {
     getMovies();
-  }, [genre, isShowing]);
+  }, [genre, isShowing, page]);
 
   return (
     <div>
-      <div className="">
+      <div>
         <select value={genre} onChange={handleGenreSelect}>
           <option value="">전체</option>
           <option value="스릴러">스릴러</option>
@@ -92,23 +107,51 @@ export default function Home() {
           <option value="true">상영중</option>
           <option value="false">비상영</option>
         </select>
+        <input
+          className="w-[200px] mt-2"
+          type="range"
+          id="range"
+          name="rating"
+          min="0"
+          max="5"
+          step="1"
+          value={minRating}
+          onChange={handleMinRatingChange}
+        />
+        <span>{minRating}</span>
         <Link href="/submit">추가하기</Link>
       </div>
-      {data.map((movie) => (
-        <div className="flex ml-2 gap-2" key={movie.id}>
-          <Link className="flex ml-2 gap-2" href={`/detail/${movie.id}`}>
-            <div>제목 : {movie.title}</div>
-            <div>장르 : {movie.genre}</div>
-            <div>개봉일 : {movie.releaseDate}</div>
-            <div>상영종료일 : {movie.endDate}</div>
-            <div>상영여부 : {movie.isShowing ? "상영중" : "상영중지"}</div>
-          </Link>
-          <Link href={`/modify/${movie.id}`}>수정</Link>
-          <button type="button" onClick={() => deleteMovie(movie.id)}>
-            삭제
-          </button>
-        </div>
-      ))}
+      {data.movies.map((movie) =>
+        movie.averageRating >= minRating ? (
+          <div className="flex ml-2 gap-2" key={movie.id}>
+            <Link className="flex ml-2 gap-2" href={`/detail/${movie.id}`}>
+              <div>제목 : {movie.title}</div>
+              <div>장르 : {movie.genre}</div>
+              <div>개봉일 : {movie.releaseDate}</div>
+              <div>상영종료일 : {movie.endDate}</div>
+              <div>상영여부 : {movie.isShowing ? "상영중" : "상영중지"}</div>
+            </Link>
+            <Link href={`/modify/${movie.id}`}>수정</Link>
+            <button type="button" onClick={() => deleteMovie(movie.id)}>
+              삭제
+            </button>
+          </div>
+        ) : null
+      )}
+      <div className="flex gap-4 ml-80">
+        <button type="button" onClick={() => setPage(1)}>
+          1
+        </button>
+        <button type="button" onClick={() => setPage(2)}>
+          2
+        </button>
+        <button type="button" onClick={() => setPage(3)}>
+          3
+        </button>
+        <button type="button" onClick={() => setPage(4)}>
+          4
+        </button>
+      </div>
     </div>
   );
 }
